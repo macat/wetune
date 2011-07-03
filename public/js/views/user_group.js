9dfc419daaf3133178ab988b06bc6f0bbcc0a683
Attila Maczak <attila@maczak.hu>
2011-07-04 00:54:12 +0200

    Bind form submit event after onload

--- public/js/views/user_group.js
+++ public/js/views/user_group.js
@@ -2,21 +2,20 @@ define(function(){
   var 
     view = {
       init: function(){
-        $('#loginform').submit(function(e){
-          e.preventDefault();
+        $(function(){
+          $('#loginform').removeClass('hide').submit(function(e){
+            e.preventDefault();
 
-          var group = $('select[name="group"]').val(), 
-              username = $('input[name="username"]').val();
+            var group = $('select[name="group"]').val(), 
+                username = $('input[name="username"]').val();
 
-          $.publish('services/wetune/login', [group, username]);
-          $('#user').text(username);
-          $('#group').text(group);
-          $('#login').addClass('hide');
-          $('#logout').removeClass('hide');
-          if (typeof this.onLogin == 'function') {
-            this.onLogin(group, username);
-          }
-          $.publish('/views/user_group/loggedin', [group, username]);
+            $.publish('services/wetune/login', [group, username]);
+            $('#user').text(username);
+            $('#group').text(group);
+            $('#login').addClass('hide');
+            $('#logout').removeClass('hide');
+            $.publish('/views/user_group/loggedin', [group, username]);
+          });
         });
       }
     };
